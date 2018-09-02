9495b891542a366001d04277e3f2c67f6d8493af
Steven Jemmott <jemmott.steven@gmail.com>
2018-09-02 00:29:03 -0400

    Designed the Hero section of website

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to my new website</h1>
-    <p>This will be my new space to showcase my work! Photography, Mobile Apps or Stories, they will be here!</p>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to my new website</h1>
+        <p>This will be my new space to showcase my work! Photography, Mobile Apps or Stories, they will be here!</p>
 
-    <Link to="/page-2/">Check out my Instagram</Link>
+        <Link to="/page-2/">Check out my Instagram</Link>
+      </div>
+    </div>
   </div>
 )
 
