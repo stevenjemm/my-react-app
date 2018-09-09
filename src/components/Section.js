242e20a2e8daa9ad0f2306ca250edb4bb9ee5078
Steven Jemmott <jemmott.steven@gmail.com>
2018-09-09 00:40:53 -0400

    Added Static data and pulled it from JSON file into Styled Components

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Wave from './Wave'
 
 const SectionGroup = styled.div`
     background: url(${props => props.image});
@@ -8,7 +9,11 @@ const SectionGroup = styled.div`
     display: grid;
     grid-template-rows: 300px auto;
     grid-gap: 20px;
+    position: relative;
 
+    @media (max-width: 640px) {
+        height: 820px;
+    }
 `
 
 const SectionLogo = styled.img`
@@ -34,14 +39,33 @@ const SectionTitle = styled.h3`
     font-size: 60px;
     margin: 0;
     line-height: 1.2;
+
+    @media (max-width: 720px) {
+        font-size: 40px;
+    }
 `
 
 const SectionText = styled.p`
     color: white;
 `
 
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: -6px;
+`
+
+const WaveTop = styled.div`
+    position: absolute;
+    width: 100%;
+    top: -6px;
+    transform: rotate(180deg);
+`
+
 const Section = props => (
     <SectionGroup image={props.image}>
+        <WaveTop><Wave /></WaveTop>
+        <WaveBottom><Wave /></WaveBottom>
         <SectionLogo src={props.logo} />
         <SectionTitleGroup>
             <SectionTitle>{props.title}</SectionTitle>
